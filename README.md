9e4bc1ac46aea415ff13887d238b4407df8ff66a
Filip <37674069+1fxe@users.noreply.github.com>
2021-03-03 00:54:59 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,6 +1,43 @@
-# ModCore Example Mod
-An example of how to use ModCore in your mods.
+<div align="center">
 
-To update the ModCore version used in your mod, grab the file from
-.minecraft/modcore called "Sk1er ModCore {version}-{mcversion}" and put it
-into the libs folder.
+# Resource Pack Display Revamp
+
+A revamp of [Canelex's Pack Display Mod](https://www.youtube.com/watch?v=LeDNOdOdGyk) with some extra features
+
+<hr/>
+
+</div>
+
+## Users
+
+### Configuration
+
+#### What is Resource Pack Display?
+
+Resource Pack Display allows you to display information about your current resource pack. <br/>
+It might look something like this:
+
+![Closer look](.github/closer.png)
+
+<details>
+  <summary>Click here to a another example</summary>
+
+![Example](.github/rdp.png)
+
+</details>
+
+#### How do I use Resource Pack Display?
+
+Resource Pack Display is installed just like any other Forge mod.
+
+If you do not know how to install a Forge mod, then we recommended looking up "How to install a Forge mod for Minecraft
+1.8.9?".
+
+#### Commands
+
+```
+/rdp        - Opens the Modcore config
+     gui    - Opens a GUI so you can drag the display to your preferred location
+     random - Chooses a random resource pack from your game and sets it
+     reset  - Resets the random resource pack duplicates list
+```
---
+++ src/main/java/dev/fxe/mods/resourcepackdisplay/ui/GUI.java
@@ -0,0 +1,75 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021. 1fxe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package dev.fxe.mods.resourcepackdisplay.ui;
+
+import dev.fxe.mods.resourcepackdisplay.data.Config;
+import net.minecraft.client.gui.GuiScreen;
+
+import java.io.IOException;
+
+/**
+ * @author Filip
+ */
+public class GUI extends GuiScreen {
+    private boolean dragging;
+    private int prevX, prevY;
+
+    @Override
+    public void initGui() {
+        super.initGui();
+    }
+
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        updatePos(mouseX, mouseY);
+        UI.drawPackDisplay();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        this.prevX = mouseX;
+        this.prevY = mouseY;
+        if (mouseButton == 0) {
+            this.dragging = true;
+        }
+    }
+
+    private void updatePos(int x, int y) {
+        if (this.dragging) {
+            Config.x = this.prevX - 10;
+            Config.y = this.prevY - 10;
+        }
+        this.prevX = x;
+        this.prevY = y;
+    }
+
+    @Override
+    protected void mouseReleased(int mouseX, int mouseY, int state) {
+        super.mouseReleased(mouseX, mouseY, state);
+        this.dragging = false;
+    }
+}
